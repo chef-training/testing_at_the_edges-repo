f6f836550fd862da4694aea1dc41edb53a814ba6
gradzi <gradzi@outbrain.com>
2017-03-02 01:45:43 +0200

    fix ServerRunner node simulation
    create a node with an assigned role and attributes

--- load_balancer/spec/unit/recipes/default_spec.rb
+++ load_balancer/spec/unit/recipes/default_spec.rb
@@ -26,28 +26,21 @@ describe 'load_balancer::default' do
     end
   end
 
-  # I could not get the ServerRunner to correctly allow me to create a role,
-  # assign that role to the node, and then have that node come back from search.
-  #
-  # I will explore this further but stick with the SoloRunner.
-  #
-
-  # context 'With a ServerRunner' do
-  #   let(:chef_run) do
-  #     runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7') do |node,server|
-  #       server.create_role('web', { default_attributes: {} })
-  #       server.create_node('node1', { :run_list => ['role:web'], 'roles' => 'web', 'cloud' => { 'public_ipv4' => '192.168.0.1' }  })
-  #       server.create_node(node1)
-  #     end
-  #     runner.converge(described_recipe)
-  #   end
-  #
-  #   it 'converges successfully' do
-  #     expect { chef_run }.to_not raise_error
-  #   end
-  #
-  #   it 'writes the correct template' do
-  #     expect(chef_run).to render_file('/etc/haproxy/haproxy.cfg').with_content('server app0 192.168.0.1:80')
-  #   end
-  # end
+  context 'With a ServerRunner' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7') do |node,server|
+        server.create_role('web', { default_attributes: {} })
+        server.create_node('node1', { :role => 'web', :normal => { 'cloud' => { 'public_ipv4' => '192.168.0.1' } } })
+      end
+      runner.converge(described_recipe)
+    end
+  
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  
+    it 'writes the correct template' do
+      expect(chef_run).to render_file('/etc/haproxy/haproxy.cfg').with_content('server app0 192.168.0.1:80')
+    end
+  end
 end
